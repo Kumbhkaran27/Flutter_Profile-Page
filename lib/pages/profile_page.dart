89a57f734c2e5cf298b9f7eee08eb6b4f13c2a17
Kumbhkaran <pankajjagtap569@gmail.com>
2024-02-18 20:31:29 -0600

    implemneted app bar fucntionality in the code and deleted the profile_heading dart file.

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../components/profile_heading.dart';
 import '../components/profile_table.dart';
 import '../components/achievements_table.dart';
 import '../components/hobbies_table.dart';
@@ -16,52 +15,61 @@ class ProfilePage extends StatelessWidget {
         body: ListView(
           physics: AlwaysScrollableScrollPhysics(),
           children: [
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Profile heading
-                  ProfileHeading(),
-
-                  // Profile information tables
-                  ProfileTable(
-                    imageUrl: 'assets/images/luffy.jpeg',
-                    name: 'Name: Luffy',
-                    sex: 'Male',
-                    age: 19,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AppBar(
+                  title: Text(
+                    'MINGLE',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 44,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                    ),
                   ),
+                  backgroundColor: Colors.white,
+                  centerTitle: true,
+                  elevation: 0,
+                ),
+                // Profile information tables
+                ProfileTable(
+                  imageUrl: 'assets/images/luffy.jpeg',
+                  name: 'Name: Luffy',
+                  sex: 'Male',
+                  age: 19,
+                ),
 
-                  // Second table with education and hometown
-                  SizedBox(height: 20), // Adding space between tables
-                  EducationAndHometownTable(
-                    education: 'Bachelor of Piracy',
-                    hometown: 'Foosha Village',
-                  ),
+                // Second table with education and hometown
+                SizedBox(height: 20), // Adding space between tables
+                EducationAndHometownTable(
+                  education: 'Bachelor of Piracy',
+                  hometown: 'Foosha Village',
+                ),
 
-                  // Hobbies table
-                  SizedBox(height: 20), // Adding space between tables
-                  HobbiesTable(
-                    hobbies: [
-                      Hobby(name: 'Sailing', imageUrl: 'assets/images/Sailing.png'),
-                      Hobby(name: 'Fishing', imageUrl: 'assets/images/Fishing.png'),
-                      Hobby(name: 'Foodie', imageUrl: 'assets/images/food.png'),
-                      Hobby(name: 'Straw Hat', imageUrl: 'assets/images/Hat.jpg'),
-                      Hobby(name: 'Fighting', imageUrl: 'assets/images/Fight.jpeg'),
-                      Hobby(name: 'Exploring', imageUrl: 'assets/images/Explore.png'),
-                    ],
-                  ),
+                // Hobbies table
+                SizedBox(height: 20), // Adding space between tables
+                HobbiesTable(
+                  hobbies: [
+                    Hobby(name: 'Sailing', imageUrl: 'assets/images/Sailing.png'),
+                    Hobby(name: 'Fishing', imageUrl: 'assets/images/Fishing.png'),
+                    Hobby(name: 'Foodie', imageUrl: 'assets/images/food.png'),
+                    Hobby(name: 'Straw Hat', imageUrl: 'assets/images/Hat.jpg'),
+                    Hobby(name: 'Fighting', imageUrl: 'assets/images/Fight.jpeg'),
+                    Hobby(name: 'Exploring', imageUrl: 'assets/images/Explore.png'),
+                  ],
+                ),
 
-                  // Achievements table
-                  SizedBox(height: 20), // Adding space between tables
-                  AchievementsTable(
-                    achievements: [
-                      Achievement(name: 'Unlocking Gear Fifth', imageUrl: 'assets/images/Gear5.jpeg'),
-                      Achievement(name: 'Mastering Advanced Conquerors Haki In A Night', imageUrl: 'assets/images/Haki.jpeg'),
-                      Achievement(name: 'Getting A Rookies Recognition From Whitebeard', imageUrl: 'assets/images/Rookie.jpeg'),
-                    ],
-                  ),
-                ],
-              ),
+                // Achievements table
+                SizedBox(height: 20), // Adding space between tables
+                AchievementsTable(
+                  achievements: [
+                    Achievement(name: 'Unlocking Gear Fifth', imageUrl: 'assets/images/Gear5.jpeg'),
+                    Achievement(name: 'Mastering Advanced Conquerors Haki In A Night', imageUrl: 'assets/images/Haki.jpeg'),
+                    Achievement(name: 'Getting A Rookies Recognition From Whitebeard', imageUrl: 'assets/images/Rookie.jpeg'),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
